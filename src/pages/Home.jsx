ba85c6a171887cd5f165766568d2052ec520c7e6
Vijeth-Kumar-18 <vijethkumar3110@gmail.com>
2025-04-17 12:17:06 +0530

    Spin Wheel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -36,7 +36,7 @@ const Home = () => {
     {
       id: 3,
       name: "Mango Magic",
-      description: "Made from the finest Alphonso mangoes, seasonal special",
+      description: "Finest Alphonso mangoes, seasonal special",
       price: "₹159",
       image: "https://images.unsplash.com/photo-1591677445540-08028eeb3021?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       rating: 4.8,
